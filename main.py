869d05d5d62d95c768916d679560cad0341fe976
ItsJules <itsjulesyt@gmail.com>
2026-03-04 21:19:33 +0800

    changed main code to do classes for now

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import json, datetime, csv, customtkinter as ctk
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+class PayrollSystem(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-app = ctk.CTk()
-app.geometry("500x300")
-app.title("Payroll System v1.0")
+        self._set_appearance_mode("dark")
+        self._set_appearance_mode("blue")
+        self.title("LPS")
+        self.geometry("1000x500")
 
-greeting_label = ctk.CTkLabel(
-    master=app,
-    text="Hello World! Welcome to the Payroll System.",
-    font=("Arial", 18, "bold")
-)
-
-greeting_label.pack(pady=40)
-
-test_button = ctk.CTkButton(
-    master=app,
-    text="Click Me",
-    command=lambda: print("Button was clicked!")
-)
-test_button.pack(pady=10)
-
-app.mainloop()
-
+if __name__ == "__main__":
+    app = PayrollSystem()
+    app.mainloop()
