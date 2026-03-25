aa4d1354ec62e05ce43b58a455ec193e942617ff
Timothy Sze <timothytimothysze@gmail.com>
2026-03-25 22:06:07 +0800

    Implemented screen updates from switching between cost & hour views, Refresh button works, Employee name dropdown works, Employee details appear

--- payroll.py
+++ payroll.py
@@ -2,16 +2,17 @@ import customtkinter as ctk
 import sqlite3 as sql
 from CTkMessagebox import CTkMessagebox as cmb
 from ctkdateentry import CTkDateEntry as DateEntry, CTkStringVar
-from datetime import datetime
+from rates import RatesManager
+from datetime import datetime, timedelta
 
 
 class PayrollFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        #self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.rm = RatesManager()
+
         self.grid_columnconfigure(0, weight=1)
-        #self.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
 
         self.header = ctk.CTkLabel(self, text="Payroll Processing", font=("Arial", 28, "bold"))
         self.header.grid(row=0, column=0, padx=20, pady=(20, 5), sticky="nw")
@@ -19,25 +20,35 @@ class PayrollFrame(ctk.CTkFrame):
         """ CONTROL FRAME """
         self.controls_frame = ctk.CTkFrame(self, fg_color="transparent")
         self.controls_frame.grid(row=1, column=0, padx=20, sticky="nsew")
-        #self.controls_frame.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
         self.main_controls_frame = ctk.CTkFrame(self.controls_frame, fg_color="transparent")
         self.main_controls_frame.grid(row=0, column=0, padx=0, sticky="nsew")
 
         # Date Entry
-        self.date_entry_label = ctk.CTkLabel(self.main_controls_frame, text="Enter the Pay Period\nStart Date:", font=("Arial", 14, "bold"))
+        self.date_entry_label = ctk.CTkLabel(self.main_controls_frame, text="Enter a Date:", font=("Arial", 14, "bold"))
         self.date_entry_label.grid(row=0, column=0, pady=(10, 0), sticky="w")
 
-        self.date_var = CTkStringVar(value="")
+        self.date_var = CTkStringVar(value=self.rm.get_today_date())
         self.date_entry = DateEntry(self.main_controls_frame, width=150, variable=self.date_var, state="readonly")
         self.date_entry.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="ew")
 
         # Employee Selection
-        self.employee_menu_label = ctk.CTkLabel(self.main_controls_frame, text="Select an employee:", font=("Arial", 14, "bold"))
+        self.employee_menu_label = ctk.CTkLabel(self.main_controls_frame, text="Select an Employee:", font=("Arial", 14, "bold"))
         self.employee_menu_label.grid(row=1, column=0, padx=(0, 10), pady=10, sticky="w")
 
+        # retrieve the list of employee names
+        conn = sql.connect("payroll_system.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT employee_name, employee_name FROM attendance GROUP BY employee_name ORDER BY employee_name ASC")
+        raw_e_names = cursor.fetchall()
+        conn.close()
+
+        e_names = []
+        for name, misc in raw_e_names:
+            e_names.append(name)
+
         self.employee_var = ctk.StringVar(value="")
-        self.employee_menu = ctk.CTkOptionMenu(self.main_controls_frame, values=["1", "2", "3"], variable=self.employee_var)
+        self.employee_menu = ctk.CTkOptionMenu(self.main_controls_frame, values=e_names, variable=self.employee_var)
         self.employee_menu.grid(row=1, column=1, padx=(0, 10), pady=10, sticky="ew")
 
         # Data View Radiobuttons
@@ -54,25 +65,35 @@ class PayrollFrame(ctk.CTkFrame):
         self.hour_view_radiobutton.grid(row=1, column=0, padx=10, pady=(15, 0), sticky="w")
 
         # Refresh Button
-        self.refresh_button = ctk.CTkButton(self.dataview_frame, text="Refresh", command='self.load_attendance')
+        self.refresh_button = ctk.CTkButton(self.dataview_frame, text="Refresh", command=self.refresh_payroll)
         self.refresh_button.grid(row=0, column=1, rowspan=2, padx=(50, 0), pady=(10, 0), sticky="ns")
 
         # Employee Details
         self.e_details_frame = ctk.CTkFrame(self.controls_frame, fg_color="transparent")
-        self.e_details_frame.grid(row=0, column=3, padx=(250, 0), sticky="e")
+        self.e_details_frame.grid(row=0, column=3, padx=(235, 0), sticky="e")
 
-        self.e_id_label = ctk.CTkLabel(self.e_details_frame, text="Employee ID: [?]", font=("Arial", 14, "bold"))
-        self.e_id_label.grid(row=0, column=0, padx=10, pady=(0, 10), sticky="e")
+        self.e_id_label = ctk.CTkLabel(self.e_details_frame, text="Employee ID:", font=("Arial", 14, "bold"))
+        self.e_id_label.grid(row=0, column=0, padx=10, pady=(0, 10), sticky="w")
 
-        self.e_role_label = ctk.CTkLabel(self.e_details_frame, text="Role: [?]", font=("Arial", 14, "bold"))
-        self.e_role_label.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="e")
+        self.e_id_var = ctk.StringVar(value="?")
+        self.e_id_display = ctk.CTkLabel(self.e_details_frame, textvariable=self.e_id_var, font=("Arial", 14, "bold"))
+        self.e_id_display.grid(row=0, column=1, padx=10, pady=(0, 10), sticky="w")
+
+        self.e_role_label = ctk.CTkLabel(self.e_details_frame, text="Role:", font=("Arial", 14, "bold"))
+        self.e_role_label.grid(row=1, column=0, padx=10, pady=(0, 10), sticky="w")
+
+        self.e_role_var = ctk.StringVar(value="?")
+        self.e_role_display = ctk.CTkLabel(self.e_details_frame, textvariable=self.e_role_var, font=("Arial", 14, "bold"))
+        self.e_role_display.grid(row=1, column=1, padx=10, pady=(0, 10), sticky="w")
 
 
         """ WEEKLY PAYROLL DATA """
-        self.weekly_frame1 = WeeklyPayrollFrame(self)
+        self.weekly_frame1 = WeeklyPayrollFrame(self, self.date_var, self.dataview_var, self.employee_var)
         self.weekly_frame1.grid(row=2, column=0, pady=(15, 0), sticky="ns")
 
-        self.weekly_frame2 = WeeklyPayrollFrame(self)
+        next_week_date = CTkStringVar(value=(datetime.strptime(self.date_var.get(), "%d/%m/%Y") + timedelta(days=7)).strftime("%d/%m/%Y"))
+
+        self.weekly_frame2 = WeeklyPayrollFrame(self, next_week_date, self.dataview_var, self.employee_var)
         self.weekly_frame2.grid(row=3, column=0, pady=(15, 0), sticky="ns")
 
 
@@ -119,13 +140,56 @@ class PayrollFrame(ctk.CTkFrame):
 
         # EXPORT PAY SLIP
         self.export_button = ctk.CTkButton(self.summary_frame, text="Export Pay Slip (.pdf)", fg_color="#1E8449",
-                                           hover_color="#196B3C", command='self.load_attendance')
+                                           hover_color="#196B3C", command='self.test')
         self.export_button.grid(row=1, column=2, padx=80, pady=(0, 10), sticky="nsew")
 
 
+        """ REFRESH DATA SETUP """
+        self.refresh_payroll()
+
+
+
+    def refresh_payroll(self):
+        '''
+
+        conn = sql.connect("payroll_system.db")
+        cursor = conn.cursor()
+
+        for i in range(7):
+            current_day = start_of_week + timedelta(days=7)
+            day_str = current_day.strftime("%Y-%m-%d")
+            cursor.execute("SELECT shift, clock_in, clock_out FROM attendance WHERE date = ?", (day_str,))
+            day_rows = cursor.fetchall()'''
+
+        # update employee details
+        if self.employee_var.get() != "":
+            conn = sql.connect("payroll_system.db")
+            cursor = conn.cursor()
+            cursor.execute("SELECT employee_id, role FROM attendance WHERE employee_name = ? GROUP BY employee_id, role",
+                           (self.employee_var.get(),))
+            e_details = cursor.fetchall()
+            conn.close()
+            #print(e_details)
+
+            self.e_id_var.set(e_details[0][0])
+            self.e_role_var.set(e_details[0][1])
+
+        # update weekly header labels and data
+        self.weekly_frame1.update_all()
+        self.weekly_frame2.update_all()
+
+
+
+
 class WeeklyPayrollFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, raw_date, dataview_mode, e_name, **kwargs):
         super().__init__(master, **kwargs)
+
+        self.raw_date = raw_date
+        self.dataview_mode = dataview_mode
+        self.e_name = e_name
+        #self.vertical_labels = vertical_labels
+        #self.horizontal_labels = date_labels
 
         #self.grid_columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
 
@@ -142,35 +206,100 @@ class WeeklyPayrollFrame(ctk.CTkFrame):
         #self.weekly_frame.grid_propagate(False)
         #self.weekly_frame.pack_propagate(False)
 
+        # DEFAULT / PLACEHOLDER VALUES
+        self.vertical_labels = {
+            "reg": ctk.StringVar(value="Regular"), "ot": ctk.StringVar(value="Overtime"), "nd": ctk.StringVar(value="Night")
+        }
+
+        self.horizontal_labels = {
+            "mon": ctk.StringVar(value="Monday"), "tue": ctk.StringVar(value="Tuesday"), "wed": ctk.StringVar(value="Wednesday"),
+            "thu": ctk.StringVar(value="Thursday"), "fri": ctk.StringVar(value="Friday"), "sat": ctk.StringVar(value="Saturday"),
+            "sun": ctk.StringVar(value="Sunday"), "total": ctk.StringVar(value="TOTAL")
+        }
+
+        self.weekly_data = {
+            "reg": {"mon": ctk.StringVar(value="1"), "tue": ctk.StringVar(value="2"), "wed": ctk.StringVar(value="3"),
+                    "thu": ctk.StringVar(value="4"), "fri": ctk.StringVar(value="5"), "sat": ctk.StringVar(value="6"),
+                    "sun": ctk.StringVar(value="7"), "total": ctk.StringVar(value="8")},
+            "ot": {"mon": ctk.StringVar(value="1"), "tue": ctk.StringVar(value="2"), "wed": ctk.StringVar(value="3"),
+                    "thu": ctk.StringVar(value="4"), "fri": ctk.StringVar(value="5"), "sat": ctk.StringVar(value="6"),
+                    "sun": ctk.StringVar(value="7"), "total": ctk.StringVar(value="8")},
+            "nd": {"mon": ctk.StringVar(value="1"), "tue": ctk.StringVar(value="2"), "wed": ctk.StringVar(value="3"),
+                    "thu": ctk.StringVar(value="4"), "fri": ctk.StringVar(value="5"), "sat": ctk.StringVar(value="6"),
+                    "sun": ctk.StringVar(value="7"), "total": ctk.StringVar(value="8")},
+        }
+
+        self.weekly_total = ctk.StringVar(value="1000")
+
+        # try:
+        selected_date = datetime.strptime(self.raw_date.get(), "%d/%m/%Y")
+        #self.db_date_str = selected_date.strftime("%Y-%m-%d")
+        # except:
+        #   return
+
+        self.start_of_week = selected_date - timedelta(days=selected_date.weekday())
+        self.db_date_str = self.start_of_week.strftime("%Y-%m-%d")
+
         self.add_header_labels()
-        self.load_cost_data()
+        self.add_placeholder_data()
 
 
     def add_header_labels(self):
-        vertical_labels = ["Regular Pay", "Overtime Pay", "Night Differential"]
-        horizontal_labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "TOTAL"] # TEMP VALUES
 
-        for row, text in enumerate(vertical_labels, 1):
-            label = ctk.CTkLabel(self.weekly_frame, text=text, font=("Arial", 16, "bold"))
+        for row, key in enumerate(self.vertical_labels, 1):
+            label = ctk.CTkLabel(self.weekly_frame, textvariable=self.vertical_labels[key], font=("Arial", 16, "bold"))
             label.grid(row=row, column=0, padx=(20, 0), pady=(0, 10), sticky="nw")
 
-        for column, text in enumerate(horizontal_labels, 1):
-            label = ctk.CTkLabel(self.weekly_frame, text=text, font=("Arial", 16, "bold"))
+        for column, key in enumerate(self.horizontal_labels, 1):
+            label = ctk.CTkLabel(self.weekly_frame, textvariable=self.horizontal_labels[key], font=("Arial", 16, "bold"))
             label.grid(row=0, column=column, padx=25, pady=10, sticky="n")
 
         weekly_total_label = ctk.CTkLabel(self.weekly_frame, text="WEEKLY TOTAL", font=("Arial", 16, "bold"))
         weekly_total_label.grid(row=4, column=6, columnspan=2, padx=25, pady=(0, 10), sticky="n")
 
 
-    def load_cost_data(self):
-        for row in range(1, 4):
-            for column in range(1, 9):
-                label = ctk.CTkLabel(self.weekly_frame, text=str(column), font=("Arial", 14))
+    def add_placeholder_data(self):
+        for row, main_key in enumerate(["reg", "ot", "nd"], 1):
+            for column, day_key in enumerate(self.weekly_data[main_key].keys(), 1):
+                label = ctk.CTkLabel(self.weekly_frame, textvariable=self.weekly_data[main_key][day_key], font=("Arial", 14))
                 label.grid(row=row, column=column, padx=25, pady=(0, 10), sticky="n")
 
-        label = ctk.CTkLabel(self.weekly_frame, text="1000", font=("Arial", 16, "bold"))
+        label = ctk.CTkLabel(self.weekly_frame, textvariable=self.weekly_total, font=("Arial", 16, "bold"))
         label.grid(row=4, column=8, padx=25, pady=(0, 10), sticky="n")
 
-    #def load_hour_data(self):
+
+    def update_all(self):
+        self.update_labels()
+        self.update_data()
 
 
+    def update_labels(self):
+        # VERTICAL LABELS
+        if self.dataview_mode.get() == "cost_view":
+            self.vertical_labels["reg"].set("Regular Pay")
+            self.vertical_labels["ot"].set("Overtime Pay")
+            self.vertical_labels["nd"].set("Night Differential")
+
+        elif self.dataview_mode.get() == "hour_view":
+            self.vertical_labels["reg"].set("Regular Hours")
+            self.vertical_labels["ot"].set("Overtime Hours")
+            self.vertical_labels["nd"].set("Night Hours")
+
+        # HORIZONTAL LABELS (DATES)
+        for i, key in enumerate(self.horizontal_labels):
+            if key == "total":
+                break
+
+            new_date = self.start_of_week + timedelta(days=i)
+            self.horizontal_labels[key].set(new_date.strftime("%b. %d"))
+
+
+    def update_data(self):
+        # need to cycle through all dates
+        conn = sql.connect("payroll_system.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT shift, clock_in, clock_out FROM attendance WHERE date = ? AND employee_name = ?",
+                       (self.db_date_str, self.e_name.get()))
+        e_details = cursor.fetchall()
+        conn.close()
+        print(e_details)
